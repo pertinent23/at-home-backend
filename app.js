8f71c104518b4af52af1878f5fe5a8e423de6439
Franck Duval <10franckpertinent@gmail.com>
2025-02-16 23:55:15 +0100

    add new origin

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({
 app.use(logger("dev"))
 
 app.use(cors({
-    origin: 'http://localhost:4200', // Autoriser les requêtes de cette origine
+    origin: ['http://localhost:4200', 'https://athome-a9287d311cf1.herokuapp.com'], // Autoriser les requêtes de cette origine
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'.split(','),
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
